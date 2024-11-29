22ec28c148f35b2981f00174bf2ce8065e7cad5c
Nimus-oes <sseo.code@gmail.com>
2024-11-29 10:12:29 +0900

    Refactored

--- basic/basic001.js
+++ basic/basic001.js
@@ -3,44 +3,40 @@ Basic 001. Display the current day and time in the following format:
 Today is Tuesday. Current time is 10 PM:30:38 
 */
 
-// Create a date object
-const dateObj = new Date();
+function getTodayDay() {
+  const dateObj = new Date();
+  // The returned values start from Sunday at 0 to Saturday at 6.
+  const day = dateObj.getDay();
 
-// Get the day of the week with the getDay() method.
-// The returned values start from Sunday at 0 to Saturday at 6.
-const day = dateObj.getDay();
+  // Create a list of days and make the order of items match the value of getDay().
+  const daysSet = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-// Create a list of days and make the order of items match the value of getDay().
-const daysSet = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+  return daysSet[day];
+}
 
-// Display the current day.
-console.log(`Today is ${daysSet[day]}`);
-
-// Get local hours data
-const hours = dateObj.getHours();
-
-// Convert the hours to 12-hour format with AM or PM period
-// Special cases (1): Noon represents PM while midnight represents AM
-// Special cases (2): Display 0 hour value as 12 (e.g. 0 AM:30:00 to 12 AM:30:00)
-function formatHours() {
+function formatHours(hours) {
   let hourNumber = hours <= 12 ? hours : hours - 12;
   hourNumber = hourNumber == 0 ? 12 : hourNumber;
   const period = hours == 24 || hours < 12 ? "AM" : "PM";
   return `${hourNumber} ${period}`;
 }
 
-const formattedHours = formatHours();
+function getCurrentTime() {
+  const dateObj = new Date();
+  const hours = dateObj.getHours();
+  const formattedHours = formatHours(hours);
+  const minutes = dateObj.getMinutes();
+  const seconds = dateObj.getSeconds();
+  return `${formattedHours}:${minutes}:${seconds}`;
+}
 
-// Get local minutes and seconds data
-const minutes = dateObj.getMinutes();
-const seconds = dateObj.getSeconds();
-
-console.log(`Current time is ${formattedHours}:${minutes}:${seconds}`);
+console.log(`Today is ${getTodayDay()}`);
+console.log(`Current time is ${getCurrentTime()}`);
