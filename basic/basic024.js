046bb73f15b7460c1c7f6d208fdb1c3104604c8e
Nimus-oes <sseo.code@gmail.com>
2024-12-05 14:38:17 +0900

    Refactored

--- basic/basic024.js
+++ basic/basic024.js
@@ -3,11 +3,13 @@ Basic 024. Find the closest value to 100 from two numerical values.
 */
 
 function getClosestTo(target, numArray) {
+  // Set the initial value of the closest number as the first item of numArray
   const closet = [Math.abs(numArray[0] - target), numArray[0]];
 
   numArray.forEach((num) => {
-    if (target - num < closet[0]) {
-      closet[0] = Math.abs(num - target);
+    const diff = Math.abs(num - target);
+    if (diff < closet[0]) {
+      closet[0] = diff;
       closet[1] = num;
     }
   });
