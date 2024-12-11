f18f3bfb82c20013b853e0b54076cb4c26bcb0e9
Nimus-oes <sseo.code@gmail.com>
2024-12-11 12:49:35 +0900

    Comment added

--- basic/basic055.js
+++ basic/basic055.js
@@ -3,6 +3,7 @@ Basic 055.
 Reverse the elements of a given array of integers of length 3.  
 */
 
+// A function that uses map function insead of reverse method
 function reverseOrder(array) {
   const lastIndex = array.length - 1;
   return array.map((element, index, arr) => arr[lastIndex - index]);
